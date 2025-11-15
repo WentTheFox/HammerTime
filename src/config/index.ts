034cd651c4e5ff70e1c991dfc2c0c2b5cc1a8e0b
WentTheFox <mail@went.tf>
2025-11-15 23:24:35 +0100

    Update source code link in footer

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import localeConfig from '../../public/locales/config.json';
 
 export const SITE_TITLE = 'HammerTime';
 export const CANONICAL_URL = 'https://hammertime.cyou';
-export const REPOSITORY_URL = 'https://github.com/DJDavid98/HammerTime';
+export const REPOSITORY_URL = 'https://github.com/DJDavid98/HammerTime/tree/legacy';
 export const CROWDIN_URL = 'https://crowdin.com/project/hammertime';
 export const FONTAWESOME_FREE_LICENSE_URL = 'https://fontawesome.com/license/free';
 
